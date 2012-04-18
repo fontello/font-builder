e69c0b13d5516b35acfab8ff5938ba601eb7d313
shkuropat <e.shkuropat@gmail.com>
2012-04-18 20:06:57 +0400

    Removed header from config generator template

--- bin/font_mkconfig.py
+++ bin/font_mkconfig.py
@@ -24,64 +24,6 @@ except:
     stderr.write("Error: fontforge can't open source font from %s" % args.src_font)
     sys.exit(1)
 
-# add comment
-config = """---
-# This is configuration file for font builder and other support scripts.
-# Format is descriped below.
-#
-#
-# demo:
-#   css_prefix: "icon-"           # prefix for css-generated classes
-#   columns: 4                    # used for html demo page generation
-#
-# font:                           # all vars from here will be used as font
-#                                 # params in fontforge
-#                                 # http://fontforge.sourceforge.net/python.html
-#   version: "1.0"
-#
-#   # use !!!small!!! letters a-z, or Opera will fail under OS X
-#   # fontname will be also used as file name.
-#   fontname: myfont
-#
-#   fullname: MyFont
-#   familyname: Myfont
-#
-#   copyright: Copyright (C) 2012 by xxxxxxxx
-#
-#   ascent: 1638
-#   descent: 410
-#   weight: Medium
-#
-#
-#
-# # Optional. You can apply global rescale + offset to all font glyphs.
-# # Param values ar 0..1, where 1 = 100%. 
-# #
-# transform:
-#   baseline: 0.2                 # baseline for rescale. Default value calculated
-#                                 # from ascent/decsent
-#   rescale: 0.68                 # rescale glyphs and center around baseline
-#   offset: -0.1                  # shift up/down
-#
-# glyphs:
-#   - file: glyph1_file           # file name, without extention
-#     from: 0xNNN                 # Symbol code 0x - hex, original
-#     code: 0xNNN                 # Symbol code 0x - hex, remapped
-#     css: icon-gpyph1-name       # For generated CSS
-#     search: [word1, word2]      # Search aliases (array). CSS name will be
-#                                 # included automatically
-#
-#     transform:                  # personal glyph transformation.
-#       rescale_rel: 0.9          # *_rel - applyed after global.
-#       offset: 0.2               # without *_rel - override global
-#
-################################################################################
-#
-# Mapping rules:
-#
-# 1. Downshift 1Fxxx -> Fxxx, because 1Fxxx codes not shown in Chrome/Opera
-#
-"""
 
 font_attrs = [
     "version",
@@ -95,7 +37,7 @@ font_attrs = [
 ]
 
 # add beginning part
-config += """
+config = """
 
 demo:
   css_prefix: "icon-"
