ae24bd641e491b7e0d0194e031e41cab6736f69e
Eugene Shkuropat <e.shkuropat@gmail.com>
2012-11-07 20:47:51 +0400

    Use phantomjs for font-dump script

--- bin/font-dump.js
+++ bin/font-dump.js
@@ -1,92 +1,149 @@
-#!/usr/bin/env node
 
 'use strict';
 
+/*global phantom, $, Raphael*/
 var fs = require('fs');
-var path = require('path');
-
-var Xml2js = require('xml2js');
-var async = require('async');
-var _ = require('underscore');
-
-var ArgumentParser = require('argparse').ArgumentParser;
-
+var system = require('system');
 
 var glyph_template = '<svg width="1000" height="1000"' +
-                     ' viewBox="0 0 1000 1000"' +
-                     ' xmlns="http://www.w3.org/2000/svg">' + "\n" +
-                     '  <g>' + "\n" +
-                     '    <path fill="currentColor" d=":d_placehoder:" />' + "\n" +
-                     '  </g>' + "\n" +
-                     '</svg>';
+                      ' viewBox="0 0 1000 1000"' +
+                      ' xmlns="http://www.w3.org/2000/svg">' + "\n" +
+                      '  <g>' + "\n" +
+                      '    <path fill="currentColor" d=":path_placehoder:" />' + "\n" +
+                      '  </g>' + "\n" +
+                      '</svg>';
 
+var GLYPH_SIZE = 1000;
 
-// fetch glyph info from general object
-//
-function prepare_glyphs(glyphs) {
-  var result = [];
-  glyphs.forEach(function (glyph) {
-    var glyph_name = glyph['$']['glyph-name'] ? glyph['$']['glyph-name'] : glyph['$']['unicode'];
-    if (_.isString(glyph_name)) {
-      glyph_name = glyph_name.replace(/^\s+|\s+$/g, "");
+var page = require('webpage').create();
+
+// jquery initialized only on html page
+page.content = '<html><body></body></html>';
+page.injectJs("../vendor/jquery-1.8.2.min.js");
+page.injectJs("../vendor/raphael-min.js");
+
+page.onConsoleMessage = function (msg) {
+  return console.log(msg);
+};
+
+page.onError = function (msg, trace) {
+  var msgStack = ["ERROR: " + msg];
+  if (trace) {
+    msgStack.push("TRACE:");
+    trace.forEach(function (t) {
+      msgStack.push(" -> " + t.file + ": " + t.line + (t.function ? " (in function '" + t.function + "')" : ""));
+    });
+  }
+  console.error(msgStack.join("\n"));
+  phantom.exit(1);
+};
+
+var src_font,
+    glyphs_dir;
+
+// simple, stupid argument parser
+// FIXME try to use argparse
+if (system.args.length === 1) {
+  console.log('Too few arguments');
+  phantom.exit(1);
+} else {
+  system.args.forEach(function (arg, i) {
+    if (arg === '-i' || arg === '--src_font') {
+      src_font = system.args[i + 1];
+      return;
     }
-    if (_.isEmpty(glyph_name)) {
+    if (arg === '-o' || arg === '--glyphs_dir') {
+      glyphs_dir = system.args[i + 1];
+      return;
+    }
+  });
+}
+if (!src_font || !glyphs_dir) {
+  console.log('Check arguments');
+  phantom.exit(1);
+}
+
+
+// read source font
+try {
+  var f = fs.open(src_font, "r");
+  var font = f.read();
+} catch (e) {
+  console.log('Cannot open ' + src_font + ': ' + e);
+  phantom.exit(1);
+}
+
+// Path glyph codes spelling
+font = font.replace(/unicode="&#x([a-f0-9]+);"/g, 'unicode="0x$1"');
+
+// fetch glyphs
+var glyphs = page.evaluate(function (font, glyph_size) {
+  var ascent = parseInt($(font).find('font-face').attr('ascent'), 10);
+  // descent has negative value
+  var descent = -parseInt($(font).find('font-face').attr('descent'), 10);
+  var font_horiz_adv_x = -$(font).find('font').attr('descent');
+
+  var result = [];
+  $(font).find('glyph').each(function () {
+    var rules, bb, tfm, scale;
+    var name, unicode;
+
+    var path = $(this).attr('d');
+    var horiz_adv_x = $(this).attr('horiz-adv-x');
+    // if gliph dosen't have own horiz-adv-x, then use font horiz-adv-x
+    if (!horiz_adv_x) {
+      horiz_adv_x = font_horiz_adv_x;
+    }
+
+    unicode = parseInt($(this).attr('unicode'), 16);
+    if (!unicode) {
       return;
     }
 
+    if (undefined !== $(this).attr('name')) {
+      name = $(this).attr('name');
+    }
+    else {
+      // if glyph name dosen't set then use unicode as file name
+      name = unicode.toString(16);
+    }
+    // trim name
+    if ('string' === typeof(name)) {
+      name = name.replace(/^\s+|\s+$/g, "");
+    }
+    if (undefined === name || null === name ||
+        '' === name) {
+      return;
+    }
+
+    //bb = Raphael.pathBBox(d);
+    scale = glyph_size / (ascent + descent);
+
+    //tfm = 's' + scale + ',-' + scale;
+    rules = [
+      // horisontal align
+      't' + ((glyph_size - horiz_adv_x) / 2) + ',0',
+      // vertical align
+      't0,' + descent,
+      // scale
+      's' + scale + ',-' + scale
+    ];
+    tfm = rules.join('');
+
     result.push({
-      name: glyph_name,
-      d: glyph['$']['d']
+      name: name,
+      path: Raphael.transformPath(path, tfm)
     });
   });
+
   return result;
-}
+}, font, GLYPH_SIZE);
 
-var parser = new ArgumentParser({
-  version: '0.0.1',
-  addHelp: true,
-  description: 'Dump glyphs from font'
+// save glyph files
+glyphs.forEach(function (glyph) {
+  f = fs.open(glyphs_dir + '/' + glyph.name + '.svg', "w");
+  f.writeLine(glyph_template.replace(':path_placehoder:', glyph.path));
+  f.close();
 });
-parser.addArgument(
-  [ '-i', '--src_font' ],
-  {
-    help: 'Source font path',
-    required: true
-  }
-);
-parser.addArgument(
-  [ '-o', '--glyphs_dir' ],
-  {
-    help: 'Glyphs output folder',
-    required: true
-  }
-);
 
-var args = parser.parseArgs();
-if (!fs.existsSync(args.src_font)) {
-  console.log('File ' + args.src_font + ' not found');
-  process.exit(1);
-}
-
-var parser = new Xml2js.Parser();
-fs.readFile(args.src_font, function (err, data) {
-  data = data.toString().replace(/unicode="&#x([a-f0-9]+);"/g, 'unicode="0x$1"');
-
-  parser.parseString(data, function (err, result) {
-    var font_horiz_adv_x = result.svg.defs[0].font[0]['$']['horiz-adv-x'];
-
-    var glyphs = prepare_glyphs(result.svg.defs[0].font[0].glyph);
-    async.forEachSeries(glyphs, function (glyph, next_glyph) {
-      var file_path = path.join(args.glyphs_dir, glyph.name + '.svg');
-      // FIXME need some magic
-      var data = glyph_template.replace(':d_placehoder:', glyph.d);
-      fs.writeFile(file_path, data, next_glyph);
-    }, function (err) {
-      if (err) {
-        console.log(err);
-        process.exit(1);
-      }
-      console.log('Done');
-    });
-  });
-});
+phantom.exit();
