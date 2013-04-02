a882b88aaab5462128dab30030a5ed8ec8823976
deepsweet <kir@soulshine.in>
2013-04-02 14:33:37 +0300

    new font-dump, first approach

--- bin/font-dump.js
+++ bin/font-dump.js
@@ -2,27 +2,18 @@
 
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-var childProcess = require('child_process');
-var crypto = require('crypto');
-
-var _ = require('underscore');
-var yaml = require('yamljs');
-var fstools = require('fs-tools');
-
-var phantomjs = require('phantomjs');
-var binPath = phantomjs.path;
-
-var svg_template =
-  '<svg height="${height}px" width="${width}px"' +
-  ' xmlns="http://www.w3.org/2000/svg">' + "\n" +
-  '  <g>' + "\n" +
-  '    <path d="${path}" />' + "\n" +
-  '  </g>' + "\n" +
-  '</svg>';
-
-var ArgumentParser = require('argparse').ArgumentParser;
+var fs = require('fs'),
+    path = require('path'),
+    crypto = require('crypto'),
+    _ = require('underscore'),
+    yaml = require('yamljs'),
+    shredder = require('../lib/shredder'),
+    svg_template =
+      '<svg height="${height}px" width="${width}px"' +
+        ' xmlns="http://www.w3.org/2000/svg">' + "\n" +
+        '    <path d="${path}" transform="${transform}" />' + "\n" +
+      '</svg>',
+    ArgumentParser = require('argparse').ArgumentParser;
 
 var parser = new ArgumentParser({
   version: '0.0.1',
@@ -73,61 +64,33 @@ parser.addArgument(
   [ '-f', '--force' ],
   {
     help: 'Force override glyphs from config',
-    action: 'storeTrue'   
+    action: 'storeTrue'
   }
 );
 
+var args = parser.parseArgs(),
+    params = {
+      src_font: args.src_font,
+      glyphs_dir: args.glyphs_dir,
+      hcrop: args.hcrop,
+      vcenter: args.vcenter,
+    };
 
-var args = parser.parseArgs();
+shredder(params, function(glyphs) {
 
-var params = {
-  src_font: args.src_font,
-  glyphs_dir: args.glyphs_dir,
-  hcrop: args.hcrop,
-  vcenter: args.vcenter,
-};
+  var config,
+      diff = [];
 
-//console.log(config.toObject());
-//process.exit();
-
-var tmp_dir = fstools.tmpdir('/tmp/font-dumpXXX');
-var tmp_config_file = path.join(tmp_dir, 'config.json');
-
-fs.mkdirSync(tmp_dir);
-fs.writeFileSync(tmp_config_file, JSON.stringify(params));
-
-var childArgs = [
-  path.join(__dirname, '../lib/font-dump-phantom.js'),
-  tmp_config_file
-];
-
-childProcess.execFile(binPath, childArgs, function (err, stdout, stderr) {
-  if (err) {
-    console.log(err);
-    fstools.removeSync(tmp_dir);
-    process.exit(1);
-  }
-
-  console.log(stdout);
-
-  // Load generated glyphs from json output
-  var glyphs = require(path.join(tmp_dir, 'config.json.out.json'));
-
-  fstools.removeSync(tmp_dir);
-
-  var config
-    , diff = [];
-  
   if (args.config) {
     config = yaml.load(args.config);
   }
 
   console.log('Writing output:\n\n');
 
-  _.each(glyphs, function(glyph) {
+  glyphs.forEach(function(glyph) {
 
-    var exists
-      , glyph_out = {};
+    var exists,
+        glyph_out = {};
 
     // if got config from existing font, then write only missed files
     if (config) {
@@ -144,6 +107,7 @@ childProcess.execFile(binPath, childArgs, function (err, stdout, stderr) {
 
     glyph.svg = svg_template
       .replace('${path}', glyph.path)
+      .replace('${transform}', glyph.transform)
       .replace('${width}', glyph.width)
       .replace('${height}', glyph.height);
 
@@ -168,6 +132,7 @@ childProcess.execFile(binPath, childArgs, function (err, stdout, stderr) {
     fs.writeFileSync(path.join(params.glyphs_dir, glyph.name + '.svg'), glyph.svg);
 
     diff.push(glyph_out);
+
   });
 
   // Create config template for new glyphs, if option set
@@ -180,4 +145,5 @@ childProcess.execFile(binPath, childArgs, function (err, stdout, stderr) {
 
     fs.writeFileSync(args.diff_config, yaml.stringify({glyphs: diff}, 10, 2));
   }
+
 });
